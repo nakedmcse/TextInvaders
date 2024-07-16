1c5d58baf3c3d86e08c813fabcbf76764c6c98b2
Walker Aldridge <daldridge@lairdscomputer.com>
2024-07-16 08:22:17 -0500

    Split types into their own header

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TextInvaders
-Text based space invaders
+Text based space invaders in C.
+
+Inspired by Dave Plumbers version on PDP.
+
+## Building:
+This uses the ncurses library, which must be installed in order to compile.
+```bash
+gcc -O3 -o textinvaders textinvaders.c -lncurses
+```
---
+++ types.h
@@ -0,0 +1,32 @@
+// Types and Defines
+#ifndef TEXTINVADER_TYPES
+#define TEXTINVADER_TYPES
+
+// Defs
+#define BULLET_UP 0
+#define BULLET_DOWN 1
+#define INVADER_LEFT -1
+#define INVADER_RIGHT 1
+#define MAX_PLAYER_BULLETS 5
+#define MAX_PLAYER_LIVES 3
+#define MAX_INVADERS 48
+
+// Types
+typedef struct bullet {
+    int oldX, oldY, X, Y;
+    bool active;
+    int direction;
+} bullet;
+
+typedef struct player {
+    int oldX, oldY, X, Y, maxX, maxY, lives, score;
+    bullet bullets[MAX_PLAYER_BULLETS];
+} player;
+
+typedef struct invader {
+    int oldX, oldY, X, Y;
+    bool active;
+    bullet bullet;
+} invader;
+
+#endif
