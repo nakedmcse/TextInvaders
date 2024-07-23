0718e296bdad690ce3d4526d4762f609a8d6edf9
Walker Aldridge <daldridge@lairdscomputer.com>
2024-07-23 08:46:32 -0500

    Make Joystick independent of keyboard

--- input.h
+++ input.h
@@ -2,55 +2,67 @@
 #ifndef TEXTINVADER_INPUT
 #define TEXTINVADER_INPUT
 
+void movePlayerLeft(player *Player) {
+    if(Player->X-1 > 2) {
+        Player->oldX = Player->X;
+        Player->X--;
+    }
+}
+
+void movePlayerRight(player *Player) {
+    if(Player->X+1 < Player->maxX-2) {
+        Player->oldX = Player->X;
+        Player->X++;
+    }
+}
+
+void spawnBullet(player *Player) {
+    int b = 0;
+    for(b = 0; b < MAX_PLAYER_BULLETS; b++) {
+        if(Player->bullets[b].active == false) {
+            Player->bullets[b].oldX = Player->X;
+            Player->bullets[b].oldY = Player->Y-1;
+            Player->bullets[b].X = Player->X;
+            Player->bullets[b].Y = Player->Y-1;
+            Player->bullets[b].active = true;
+            Player->bullets[b].direction = BULLET_UP;
+            break;
+        }
+    }
+}
+
 bool pollInput(player *Player, SDL_Joystick *joystick, int frame_timer) {
     int ch = getch();
-    int b = 0;
     bool quit = false;
 
     if(joystick) {
         SDL_Event joyEvt;
         while (SDL_PollEvent(&joyEvt)) {
             if (joyEvt.type == SDL_JOYBUTTONDOWN) {
-                ch = ' '; // pass fire to action
+                spawnBullet(Player);
             }
         }
 
         int xaxis = SDL_JoystickGetAxis(joystick, 0);
         if (xaxis < -8000 & frame_timer == 0) {
-            ch = 'z';
+            movePlayerLeft(Player);
         } else if (xaxis > 8000 & frame_timer == 0) {
-            ch = 'x';
+            movePlayerRight(Player);
         }
     }
 
     switch((char)ch) {
     case 'z':
-        if(Player->X-1 > 2) {
-            Player->oldX = Player->X;
-            Player->X--;
-        }
+        movePlayerLeft(Player);
         break;
     case 'x':
-        if(Player->X+1 < Player->maxX-2) {
-            Player->oldX = Player->X;
-            Player->X++;
-        }
+        movePlayerRight(Player);
         break;
     case 'q':
         quit = true;
         break;
     case ' ':
-        for(b = 0; b < MAX_PLAYER_BULLETS; b++) {
-            if(Player->bullets[b].active == false) {
-                Player->bullets[b].oldX = Player->X;
-                Player->bullets[b].oldY = Player->Y-1;
-                Player->bullets[b].X = Player->X;
-                Player->bullets[b].Y = Player->Y-1;
-                Player->bullets[b].active = true;
-                Player->bullets[b].direction = BULLET_UP;
-                break;
-            }
-        }
+        spawnBullet(Player);
         break;
     }
 
